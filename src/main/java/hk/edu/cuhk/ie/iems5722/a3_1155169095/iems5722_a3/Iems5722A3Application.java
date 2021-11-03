b1c2f6672280f3fa401ac4847e96e7dd4a693e02
xuefeng1123 <1827699942@qq.com>
2021-11-03 15:26:31 +0800

    first commit

---
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a3_1155169095/iems5722_a3/Iems5722A3Application.java
@@ -0,0 +1,13 @@
+package hk.edu.cuhk.ie.iems5722.a3_1155169095.iems5722_a3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iems5722A3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iems5722A3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hk/edu/cuhk/ie/iems5722/a3_1155169095/iems5722_a3/Iems5722A3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hk.edu.cuhk.ie.iems5722.a3_1155169095.iems5722_a3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iems5722A3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
