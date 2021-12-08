947cd450e6e330979ead29829555250b656b0e36
xuefeng1123 <1827699942@qq.com>
2021-12-08 20:20:48 +0800

    assignment4:added notification func to multiple devices (withBUG)

---
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a3_1155169095/iems5722_a3/Entity/FBNotification.java
@@ -0,0 +1,2 @@
+package hk.edu.cuhk.ie.iems5722.a3_1155169095.iems5722_a3.Entity;public class FBNotification {
+}
--- src/main/java/hk/edu/cuhk/ie/iems5722/a3_1155169095/iems5722_a3/Service/FCMService.java
+++
@@ -1,4 +0,0 @@
-package hk.edu.cuhk.ie.iems5722.a3_1155169095.iems5722_a3.Service;
-
-public class FCMService {
-}
